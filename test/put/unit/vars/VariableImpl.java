0bc7b384b9689c1583c61f2b15fc30ef03c3cd1a
Konrad Siek <konrad.siek@cs.put.edu.pl>
2014-08-08 13:45:43 +0200

    Eclipse feels that should be done differently.
    Who am I to argue?

--- test/put/unit/vars/VariableImpl.java
+++ test/put/unit/vars/VariableImpl.java
@@ -1,11 +1,14 @@
 package put.unit.vars;
 
+import static put.atomicrmi.Access.Mode.ANY;
+import static put.atomicrmi.Access.Mode.READ_ONLY;
+import static put.atomicrmi.Access.Mode.WRITE_ONLY;
+
 import java.rmi.RemoteException;
 import java.util.List;
 
 import put.atomicrmi.Access;
 import put.atomicrmi.TransactionalUnicastRemoteObject;
-import static put.atomicrmi.Access.Mode.*;
 
 public class VariableImpl extends TransactionalUnicastRemoteObject implements Variable, Cloneable {
 
