c4b2d8abfebdc819c00ee49848c97efc8d00e6f3
Konrad Siek <konrad.siek@cs.put.edu.pl>
2014-11-26 11:32:22 +0100

    Missing test.

---
+++ test/put/unit/generic/ManualEarlyReleaseOfUnused.java
@@ -0,0 +1,117 @@
+package put.unit.generic;
+
+import java.rmi.RemoteException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import put.atomicrmi.Transaction;
+import put.atomicrmi.TransactionFailureMonitor;
+import put.unit.RMITest;
+import put.unit.vars.Variable;
+import edu.umd.cs.mtc.MultithreadedTest;
+import edu.umd.cs.mtc.TestFramework;
+
+/**
+ * Manual early release test case.
+ * 
+ * <pre>
+ * T1 [ man_release(x) r(y)0 w(y)1 ]
+ * T2  [               r(x)0 w(x)1 ]
+ * </pre>
+ */
+public class ManualEarlyReleaseOfUnused extends RMITest {
+	class Threads extends MultithreadedTest {
+
+		AtomicInteger aint;
+
+		@Override
+		public void initialize() {
+			aint = new AtomicInteger(0);
+		}
+
+		public void thread1() {
+			Transaction t = null;
+			try {
+				t = new Transaction();
+				Variable x = t.accesses((Variable) registry.lookup("x"));
+				Variable y = t.accesses((Variable) registry.lookup("y"));
+
+				t.start();
+				waitForTick(1);
+				waitForTick(2);
+
+				t.release(x);
+
+				int vy = y.read();
+				y.write(vy + 1);
+
+				waitForTick(3);
+
+				// If manual early release worked correctly, T2 should be free
+				// to set aint to 1 before tick 3.
+				Assert.assertEquals(1, aint.get());
+
+				t.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			} finally {
+				t.stopHeartbeat();
+			}
+
+			waitForTick(99);
+			try {
+				TransactionFailureMonitor.getInstance().emergencyStop();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+		}
+
+		public void thread2() {
+			Transaction t = null;
+			try {
+				t = new Transaction();
+				Variable x = t.accesses((Variable) registry.lookup("x"));
+
+				waitForTick(1);
+				t.start();
+				waitForTick(2);
+
+				int vx = x.read();
+				x.write(vx + 1);
+
+				aint.set(1);
+
+				waitForTick(3);
+
+				t.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			} finally {
+				t.stopHeartbeat();
+			}
+
+			waitForTick(99);
+			try {
+				TransactionFailureMonitor.getInstance().emergencyStop();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+		}
+	}
+
+	@Test
+	public void manualEarlyRelease() throws Throwable {
+		TestFramework.runOnce(new Threads());
+
+		Assert.assertEquals(1, state("x"));
+		Assert.assertEquals(1, state("y"));
+	}
+}
