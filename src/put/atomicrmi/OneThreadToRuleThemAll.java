c8118e8fe10d95d8f35e0189dad007795b1ee575
Konrad Siek <konrad.siek@cs.put.edu.pl>
2015-07-03 13:06:13 +0200

    Emergency stop for OneThread

--- src/put/atomicrmi/OneThreadToRuleThemAll.java
+++ src/put/atomicrmi/OneThreadToRuleThemAll.java
@@ -2,8 +2,11 @@ package put.atomicrmi;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
+import put.atomicrmi.OneThreadToRuleThemAll.Task;
 
 public class OneThreadToRuleThemAll extends Thread {
 
@@ -16,6 +19,10 @@ public class OneThreadToRuleThemAll extends Thread {
 	private final Map<Object, Set<Task>> tasks = new HashMap<Object, Set<Task>>();
 
 	private final Set<Object> activeCategories = new HashSet<Object>();
+
+	private int taskCount = 0;
+
+	private boolean run = true;
 
 	/**
 	 * Controller thread is waiting, as opposed to executing some task.
@@ -31,6 +38,8 @@ public class OneThreadToRuleThemAll extends Thread {
 	}
 
 	public synchronized boolean add(Object category, Task task) {
+		System.out.println("Adding " + category + " task " + task);
+
 		Set<Task> set = tasks.get(category);
 		if (set == null) {
 			set = new HashSet<Task>();
@@ -38,6 +47,7 @@ public class OneThreadToRuleThemAll extends Thread {
 		}
 
 		set.add(task);
+		taskCount++;
 
 		activeCategories.add(category);
 		notify();
@@ -46,42 +56,89 @@ public class OneThreadToRuleThemAll extends Thread {
 	}
 
 	public synchronized void ping(Object category) {
+		System.out.println("Pinging " + category);
+
 		activeCategories.add(category);
 		notify();
 	}
+	
+	public synchronized void emergencyStop() {
+		if (!isAlive()) {
+			return;
+		}
+		run  = false;
+		interrupt();
+	}
+
+	// private boolean activeCategoriesPresent() {
+	// for (Set<Object> category : activeCategories) {
+	// if (!category.isEmpty())
+	// return true;
+	// }
+	// return false;
+	// }
 
 	@Override
 	public void run() {
 
-		while (true) {
+		while (run) {
+			System.out.println("Looping in OneThread");
+
 			/**
 			 * If nobody is doing anything, wait for someone to deposit some
 			 * work or ping.
 			 */
 			synchronized (this) {
 				try {
-					if (activeCategories.isEmpty())
+					if (taskCount == 0 || activeCategories.isEmpty()) { // TODO
+						System.out.println("Waiting in OneThread");
 						wait();
+					} else {
+						System.out.println("Not waiting in OneThread - active categories present");
+					}
 				} catch (InterruptedException e) {
 					// Intentionally left blank.
+					System.out.println("OneThread interrupted");
+					if (!run) {
+						break;
+					}
 				}
 			}
+
+			System.out.println("Activating OneThread");
 
 			/**
 			 * Somebody woke us up, we see whom it was, and check his task.
 			 */
-			synchronized (this) {
-				for (Object category : activeCategories) {
-					for (Task task : tasks.get(category)) {
-						try {
-							if (task.condition(this))
-								task.run(this);
-						} catch (Exception e) {
-							throw new RuntimeException(e.getMessage(), e.getCause());
+			// synchronized (this) {
+			Iterator<Object> categoryIterator = activeCategories.iterator();
+			while (categoryIterator.hasNext()) {
+				Object category = categoryIterator.next();
+				categoryIterator.remove();
+
+				System.out.println("Going through category " + category + ": " + tasks.get(category));
+
+				Iterator<Task> taskIterator = tasks.get(category).iterator();
+				while (taskIterator.hasNext()) {
+					Task task = taskIterator.next();
+					System.out.println("Going through category " + category + " task " + task);
+					try {
+						if (task.condition(this)) {
+							System.out.println("Condition met for task " + task);
+							taskIterator.remove();
+							task.run(this);
+							taskCount--;
+						} else {
+							System.out.println("Condition not met for task " + task);
 						}
+					} catch (Exception e) {
+						throw new RuntimeException(e.getMessage(), e.getCause());
 					}
 				}
 			}
+			// }
+
+			System.out.println("OneThread going to sleep");
 
 			/**
 			 * Aaand... go back to waiting.
