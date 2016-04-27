a09271dc69aa71fb8db82807d1e85a62536c8c50
Konrad Siek <konrad.siek@cs.put.edu.pl>
2016-04-27 11:52:09 +0200

    Unit test.

---
+++ test/put/unit/irrevocable/PreventCascade.java
@@ -0,0 +1,127 @@
+package put.unit.irrevocable;
+
+import java.rmi.RemoteException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import put.atomicrmi.optsva.RollbackForcedException;
+import put.atomicrmi.optsva.Transaction;
+import put.atomicrmi.optsva.Transaction.Type;
+import put.atomicrmi.optsva.sync.Heartbeat;
+import put.atomicrmi.optsva.sync.TaskController;
+import put.atomicrmi.optsva.sync.TransactionFailureMonitorImpl;
+import put.unit.RMITest;
+import put.unit.vars.Variable;
+import edu.umd.cs.mtc.MultithreadedTest;
+import edu.umd.cs.mtc.TestFramework;
+
+/**
+ * Forced abort test case.
+ * 
+ * <pre>
+ * T1 [   r(x)0 w(x)1              r(y)0 w(y)1 r(z)0 w(z)1              !
+ * T2   [             r(x)----------------------------------------------1 w(x)1              r(y)0 w(y)1 r(z)0 w(z)1    !
+ * </pre>
+ */
+public class PreventCascade extends RMITest {
+	class Threads extends MultithreadedTest {
+		public void thread1() {
+			Transaction t = null;
+			try {
+				t = new Transaction();
+				Variable x = t.accesses((Variable) registry.lookup("x"), 2);
+				Variable y = t.accesses((Variable) registry.lookup("y"), 2);
+				Variable z = t.accesses((Variable) registry.lookup("z"), 2);
+
+				t.start();
+				waitForTick(1);
+
+				waitForTick(2);
+				int vx = x.read();
+				x.write(vx + 1);
+
+				waitForTick(3);
+				int vy = y.read();
+				y.write(vy + 1);
+
+				int vz = z.read();
+				z.write(vz + 1);
+
+				waitForTick(4);
+				waitForTick(5);
+				t.rollback();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			} 
+
+			waitForTick(99);
+			try {
+				TransactionFailureMonitorImpl.getInstance().emergencyStop();
+				TaskController.emergencyStop();
+				Heartbeat.emergencyStop();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+		}
+
+		public void thread2() {
+			Transaction t = null;
+			try {
+				int v;
+				t = new Transaction(Type.Irrevocable);
+				Variable x = t.accesses((Variable) registry.lookup("x"), 2);
+				Variable y = t.accesses((Variable) registry.lookup("y"), 2);
+				Variable z = t.accesses((Variable) registry.lookup("z"), 2);
+
+				waitForTick(1);
+				t.start();
+
+				waitForTick(2);
+				v = x.read();
+				x.write(v + 1);
+
+				// waitForTick(3);
+				v = y.read();
+				y.write(v + 1);
+
+				v = z.read();
+				z.write(v + 1);
+
+				//waitForTick(4);
+				t.commit();
+				//waitForTick(5);
+
+			} catch (RollbackForcedException e) {
+				Assert.fail("Transaction aborted when it should have committed.");
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+
+			waitForTick(99);
+			try {
+				TransactionFailureMonitorImpl.getInstance().emergencyStop();
+				TaskController.emergencyStop();
+				Heartbeat.emergencyStop();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+		}
+	}
+
+	@Test
+	public void preventCascade() throws Throwable {
+		TaskController.emergencyStart();
+		Heartbeat.emergencyStart();
+		TestFramework.runOnce(new Threads());
+
+		Assert.assertEquals(1, state("x"));
+		Assert.assertEquals(1, state("y"));
+		Assert.assertEquals(1, state("z"));
+	}
+}
